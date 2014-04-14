69858b3333ed1afe2ad4d25bf72ed35c976c290c
Donald Averill <donaldaverill@gmail.com>
2014-04-14 07:45:23 -0700

    Reconfiguring files

--- package.js
+++ package.js
@@ -1,7 +1,9 @@
 Package.describe({
     summary: 'serialport repackaged for Meteor'
 });
-
+Npm.depends({
+    "serialport": "1.3.1"
+});
 Package.on_use(function(api) {
     api.add_files('serialport.js', ['server']);
     api.export('SerialPort', 'server');
