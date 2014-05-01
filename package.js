8a09cfb07187d23e24670ab6b332594937394dd5
Donald Averill <donaldaverill@gmail.com>
2014-04-30 23:05:24 -0700

    changing to serial port version 1.4.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     summary: 'serialport repackaged for Meteor'
 });
 Npm.depends({
-    "serialport": "1.3.1"
+    "serialport": "1.4.0"
 });
 Package.on_use(function(api) {
     api.add_files('serialport.js', ['server']);
